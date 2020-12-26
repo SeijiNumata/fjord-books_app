3d16abb34cd83260b8b306cd001cbee591f787a0
SeijiNumata <seizi.numata@gmail.com>
2020-12-26 23:25:19 +0900

    showが完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
-  root to: "home#index"
+  resources :users, only: [:show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
