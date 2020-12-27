d3a745cce98885ebc239fe3145bc901e24e51919
SeijiNumata <seizi.numata@gmail.com>
2020-12-27 21:50:35 +0900

    showの完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
+  get '/' => redirect('/books')
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show,:index]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
