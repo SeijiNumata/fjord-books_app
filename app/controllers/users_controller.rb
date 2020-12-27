cde0d3e8e946746292f96d065fc506d71eaa6607
SeijiNumata <seizi.numata@gmail.com>
2020-12-27 23:26:52 +0900

    多分出来た

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only:[:mypage, :edit, :update]
+
   def index
     @users=User.all
   end
@@ -7,10 +9,32 @@ class UsersController < ApplicationController
   def show
   end
 
+  def mypage
+    redirect_to user_path(current_user)
+  end
+
+  def edit
+    unless @user == current_user
+      redirect_to user_path(@user)
+      end
+  end
+
+def update
+  if current_user.update(user_params)
+     redirect_to user_path(current_user)
+  else
+     redirect_to edit_user_path(current_user)
+  end
+end
+
   private
 
   def set_user
     @user = User.find(params[:id])
   end
 
+  def user_params
+      # params.fetch(:user, {}).permit(:user_name)
+      params.fetch(:user, {}).permit(:user_name,:introduce_yourself,:postal_code,:address)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'users/show'
   get '/' => redirect('/books')
   devise_for :users
-  resources :users, only: [:show,:index]
+  get '/mypage' => 'users#mypage'
+  resources :users, only: [:show,:index,:edit, :update]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
