717053ded2ece0cae850603ccc0abef4b8618d32
SeijiNumata <seizi.numata@gmail.com>
2021-01-01 18:20:38 +0900

    rubocop適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-  def after_sign_in_path_for(resource)
-    user_path(resource.id)
-  end
+
   protected
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:account_update, keys: [:user_name,:postal_code,:address,:introduce_yourself])
-end
-def after_sign_in_path_for(resource)
-  books_path
-end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[user_name postal_code address introduce_yourself])
+  end
 
-def after_sign_out_path_for(resource)
-  books_path
-end
+  def after_sign_in_path_for(_resource)
+    books_path
+  end
 
+  def after_sign_out_path_for(_resource)
+    books_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
-  def show
-  end
+  def show; end
 
   def index
-    @users=User.all
+    @users = User.all
   end
 
   private
