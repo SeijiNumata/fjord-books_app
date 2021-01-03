0e47dbe331fa2d89276dc725f6c2870416a5df02
SeijiNumata <seizi.numata@gmail.com>
2021-01-03 13:58:22 +0900

    一旦完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
-
+  has_one_attached :image
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.name = auth.info.name
