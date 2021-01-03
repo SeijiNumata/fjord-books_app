58b5a818a15fbfb114925f337e71358ef6702692
SeijiNumata <seizi.numata@gmail.com>
2021-01-03 23:52:06 +0900

    フォロー、fフォロワーの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.following
+ end
+
+ def followers
+  @user  = User.find(params[:id])
+  puts "龍#{@user}"
+  @users = @user.followers
+end
+
 end
