cf98b4de78d36dab33d8c052c7c04fd882c89210
SeijiNumata <seizi.numata@gmail.com>
2021-01-08 22:40:59 +0900

    rubocop

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,10 @@
 
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
-  
+
   # GET /books
   # GET /books.json
-  PER_ONE_PAGE=5
+  PER_ONE_PAGE = 5
   def index
     @books = Book.page(params[:page]).per(PER_ONE_PAGE).order(:id)
   end
