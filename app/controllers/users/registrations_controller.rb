63d3c0cae3ebba543a430f1c69783ac254f5cd21
SeijiNumata <seizi.numata@gmail.com>
2021-01-10 18:46:25 +0900

    rubocop

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,6 +11,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
     return super if params['password']&.present?
+
     # 現在のパスワードなしでアカウントの更新をする
     resource.update_without_password(params.except('current_password'))
   end
