70dcd06d5d20b887c61a202571eba5d165454d66
SeijiNumata <seizi.numata@gmail.com>
2021-01-13 22:21:54 +0900

    コメント欄にpagingの対応
    kaminariでコメント欄をpagingできるようにしました。

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,7 +6,7 @@ class ReportsController < ApplicationController
 
   # GET /reports
   def index
-    @reports = Report.all
+    @reports = Report.all.order(:id).page(params[:page])
   end
 
   # GET /reports/1
@@ -14,7 +14,7 @@ class ReportsController < ApplicationController
     #  @comment=@report.comments.build(user_id: current_user.id)
     @comment = Comment.new
     #  @comments=@report.comments.all
-    @comments = @report.comments.includes(:user)
+    @comments = @report.comments.includes(:user).order(:id).page(params[:page])
   end
 
   # GET /reports/new
