180e1c0101d117c4e79614d3fe215b2f221cf810
SeijiNumata <seizi.numata@gmail.com>
2021-01-13 22:18:37 +0900

    kaminari対応

--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -3,6 +3,6 @@
 class Users::FollowersController < ApplicationController
   def index
     @user  = User.find(params[:user_id])
-    @users = @user.followers
+    @users = @user.followers.order(id: :desc).page(params[:page])
   end
 end
