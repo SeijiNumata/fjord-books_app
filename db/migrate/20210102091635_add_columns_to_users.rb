45d234a953193d1d244037f1d5f9c8210b798783
SeijiNumata <seizi.numata@gmail.com>
2021-01-16 19:27:38 +0900

    レビューを反映

--- db/migrate/20210102091635_add_columns_to_users.rb
+++ db/migrate/20210102091635_add_columns_to_users.rb
@@ -2,8 +2,8 @@
 
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :uid, :string, null: false, default: ''
+    add_column :users, :uid, :string, null: false,unique:true
     add_column :users, :provider, :string, null: false, default: ''
-    add_index :users, %i[uid provider], unique: true
+    add_index :users, %i[uid], unique: true
   end
 end
