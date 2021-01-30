b16d0e0aba8981c86493155603c99d783ea48234
SeijiNumata <seizi.numata@gmail.com>
2021-01-30 09:57:16 +0900

    Rubocopの修正（フォーマットを綺麗にしました)
    一箇所不要なスペースがあるとRubocopに指摘されたので直しました。

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -47,7 +47,7 @@ class ReportsController < ApplicationController
   # DELETE /reports/1
   def destroy
     @report.destroy
-    redirect_to reports_url, notice:  t('controllers.common.notice_destroy', name: Report.model_name.human)
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
